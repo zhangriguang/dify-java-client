9816504f21b5ec4b8a52612cb6a260655616f8a0
zhangrg <zhangrg@create80.com>
2025-05-09 09:03:51 +0800

    feat:增加支持Dify1.3.1版本后的标注功能模块接口！

--- src/main/java/io/github/imfangs/dify/client/DifyChatClient.java
+++ src/main/java/io/github/imfangs/dify/client/DifyChatClient.java
@@ -20,7 +20,7 @@ public interface DifyChatClient extends DifyBaseClient {
      *
      * @param message 消息
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     ChatMessageResponse sendChatMessage(ChatMessage message) throws IOException, DifyApiException;
@@ -31,7 +31,7 @@ public interface DifyChatClient extends DifyBaseClient {
      *
      * @param message  消息
      * @param callback 回调
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     void sendChatMessageStream(ChatMessage message, ChatStreamCallback callback) throws IOException, DifyApiException;
@@ -42,7 +42,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param taskId 任务 ID
      * @param user   用户标识
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     SimpleResponse stopChatMessage(String taskId, String user) throws IOException, DifyApiException;
@@ -55,7 +55,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param user      用户标识
      * @param content   内容
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     SimpleResponse feedbackMessage(String messageId, String rating, String user, String content) throws IOException, DifyApiException;
@@ -66,7 +66,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param messageId 消息 ID
      * @param user      用户标识
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     SuggestedQuestionsResponse getSuggestedQuestions(String messageId, String user) throws IOException, DifyApiException;
@@ -79,7 +79,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param firstId        当前页第一条聊天记录的 ID
      * @param limit          一次请求返回多少条聊天记录
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     MessageListResponse getMessages(String conversationId, String user, String firstId, Integer limit) throws IOException, DifyApiException;
@@ -92,7 +92,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param limit  一次请求返回多少条记录
      * @param sortBy 排序字段
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     ConversationListResponse getConversations(String user, String lastId, Integer limit, String sortBy) throws IOException, DifyApiException;
@@ -103,7 +103,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param conversationId 会话 ID
      * @param user           用户标识
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     SimpleResponse deleteConversation(String conversationId, String user) throws IOException, DifyApiException;
@@ -116,7 +116,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param autoGenerate   自动生成标题
      * @param user           用户标识
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     Conversation renameConversation(String conversationId, String name, Boolean autoGenerate, String user) throws IOException, DifyApiException;
@@ -127,7 +127,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param file 文件
      * @param user 用户标识
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     AudioToTextResponse audioToText(File file, String user) throws IOException, DifyApiException;
@@ -139,7 +139,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param fileName    文件名
      * @param user        用户标识
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     AudioToTextResponse audioToText(InputStream inputStream, String fileName, String user) throws IOException, DifyApiException;
@@ -151,7 +151,7 @@ public interface DifyChatClient extends DifyBaseClient {
      * @param text      文本
      * @param user      用户标识
      * @return 音频数据
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     byte[] textToAudio(String messageId, String text, String user) throws IOException, DifyApiException;
@@ -160,9 +160,76 @@ public interface DifyChatClient extends DifyBaseClient {
      * 获取应用元数据
      *
      * @return 响应
-     * @throws IOException IO异常
+     * @throws IOException      IO异常
      * @throws DifyApiException API异常
      */
     AppMetaResponse getAppMeta() throws IOException, DifyApiException;
 
+    /**
+     * 获取标注列表
+     *
+     * @param page  页码
+     * @param limit 每页数量
+     * @return 响应
+     * @throws IOException      IO异常
+     * @throws DifyApiException API异常
+     */
+    AnnotationListResponse getAnnotations(Integer page, Integer limit) throws IOException, DifyApiException;
+
+    /**
+     * 创建标注
+     *
+     * @param question 问题
+     * @param answer   答案内容
+     * @return 标注
+     * @throws IOException      IO异常
+     * @throws DifyApiException API异常
+     */
+    Annotation saveAnnotation(String question, String answer) throws IOException, DifyApiException;
+
+    /**
+     * 更新标注
+     *
+     * @param annotationId 标注 ID
+     * @param question     问题
+     * @param answer       答案内容
+     * @return 标注
+     * @throws IOException      IO异常
+     * @throws DifyApiException API异常
+     */
+    Annotation updateAnnotation(String annotationId, String question, String answer) throws IOException, DifyApiException;
+
+    /**
+     * 删除标注
+     *
+     * @param annotationId 标注 ID
+     * @return 响应
+     * @throws IOException      IO异常
+     * @throws DifyApiException API异常
+     */
+    SimpleResponse deleteAnnotation(String annotationId) throws IOException, DifyApiException;
+
+    /**
+     * 标注回复初始设置
+     *
+     * @param action                动作，只能是 'enable' 或 'disable'
+     * @param embeddingProviderName 指定的嵌入模型提供商, 必须先在系统内设定好接入的模型，对应的是provider字段
+     * @param embeddingModelName    指定的嵌入模型，对应的是model字段
+     * @param scoreThreshold        相似度阈值，当相似度大于该阈值时，系统会自动回复，否则不回复
+     * @return 标注回复
+     * @throws IOException      IO异常
+     * @throws DifyApiException API异常
+     */
+    AnnotationReply annotationReply(String action, String embeddingProviderName, String embeddingModelName, Integer scoreThreshold) throws IOException, DifyApiException;
+
+    /**
+     * 查询标注回复初始设置任务状态
+     *
+     * @param action 动作，只能是 'enable' 或 'disable'，并且必须和标注回复初始设置接口的动作一致
+     * @param jobId  任务 ID，从标注回复初始设置接口返回的 jobId
+     * @return 标注回复
+     * @throws IOException      IO异常
+     * @throws DifyApiException API异常
+     */
+    AnnotationReply getAnnotationReply(String action, String jobId) throws IOException, DifyApiException;
 }
