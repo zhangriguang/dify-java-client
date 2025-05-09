ae2be835fb7e128cda56a6c76c4a4221ff2c81c2
zhangrg <zhangrg@create80.com>
2025-05-09 10:27:16 +0800

    feat:增加支持Dify1.3.1版本后的标注功能模块接口测试用例！

--- src/test/java/io/github/imfangs/dify/client/DifyBaseClientTest.java
+++ src/test/java/io/github/imfangs/dify/client/DifyBaseClientTest.java
@@ -2,9 +2,8 @@ package io.github.imfangs.dify.client;
 
 import io.github.imfangs.dify.client.config.DifyTestConfig;
 import io.github.imfangs.dify.client.model.DifyConfig;
-import io.github.imfangs.dify.client.model.chat.AppInfoResponse;
-import io.github.imfangs.dify.client.model.chat.AppMetaResponse;
-import io.github.imfangs.dify.client.model.chat.AppParametersResponse;
+import io.github.imfangs.dify.client.model.chat.*;
+import io.github.imfangs.dify.client.model.common.SimpleResponse;
 import io.github.imfangs.dify.client.model.file.FileUploadRequest;
 import io.github.imfangs.dify.client.model.file.FileUploadResponse;
 import okhttp3.MediaType;
@@ -14,6 +13,8 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Dify 基础客户端测试类
@@ -131,6 +132,99 @@ public class DifyBaseClientTest {
         try (DifyClient client = DifyClientFactory.createClient(config)) {
             AppInfoResponse appInfo = client.getAppInfo();
             System.out.println("应用信息: " + appInfo);
+        }
+    }
+
+    /**
+     * 测试获取标注列表
+     */
+    @Test
+    public void testGetAnnotations() throws Exception {
+        try (DifyClient client = DifyClientFactory.createClient(BASE_URL, API_KEY)) {
+            int page = 1;
+            int limit = 10;
+            AnnotationListResponse list = client.getAnnotations(page, limit);
+            System.out.println("标注列表数据: " + list);
+        }
+    }
+
+    /**
+     * 测试创建标注
+     */
+    @Test
+    public void testSaveAnnotation() throws Exception {
+        try (DifyClient client = DifyClientFactory.createClient(BASE_URL, API_KEY)) {
+            String question = "创建你的问题";
+            String answer = "创建问题回答";
+            Annotation annotation = client.saveAnnotation(question, answer);
+            System.out.println("保存标注数据: " + annotation);
+        }
+    }
+
+    /**
+     * 测试更新标注
+     */
+    @Test
+    public void testUpdateAnnotation() throws Exception {
+        try (DifyClient client = DifyClientFactory.createClient(BASE_URL, API_KEY)) {
+            int page = 1;
+            int limit = 1;
+            AnnotationListResponse list = client.getAnnotations(page, limit);
+            List<Annotation> data = list.getData();
+            if(Objects.nonNull(data)) {
+                String  annotationId = data.get(0).getId();
+                String question = "更新你的问题";
+                String answer = "更新问题回答";
+                Annotation annotation = client.updateAnnotation(annotationId, question, answer);
+                System.out.println("更新标注数据: " + annotation);
+            }
+        }
+    }
+
+    /**
+     * 测试删除标注
+     */
+    @Test
+    public void testDeleteAnnotation() throws Exception {
+        try (DifyClient client = DifyClientFactory.createClient(BASE_URL, API_KEY)) {
+            int page = 1;
+            int limit = 1;
+            AnnotationListResponse list = client.getAnnotations(page, limit);
+            List<Annotation> data = list.getData();
+            if(Objects.nonNull(data)) {
+                String  annotationId = data.get(0).getId();
+                SimpleResponse response = client.deleteAnnotation(annotationId);
+                System.out.println("删除标注数据: " + response);
+            }
+        }
+    }
+
+    /**
+     * 测试标注回复初始设置
+     */
+    @Test
+    public void testAnnotationReply() throws Exception {
+        try (DifyClient client = DifyClientFactory.createClient(BASE_URL, API_KEY)) {
+            String action = "enable";
+            String embeddingProviderName = "指定的嵌入模型提供商";
+            String embeddingModelName = "指定的嵌入模型";
+            Integer scoreThreshold = 10;
+            AnnotationReply annotationReply = client.annotationReply(action, embeddingProviderName, embeddingModelName, scoreThreshold);
+            System.out.println("标注回复初始设置数据: " + annotationReply);
+
+        }
+    }
+
+    /**
+     * 测试查询标注回复初始设置任务状态
+     */
+    @Test
+    public void testGetAnnotationReply() throws Exception {
+        try (DifyClient client = DifyClientFactory.createClient(BASE_URL, API_KEY)) {
+            String action = "disable";
+            String jobId = "任务 ID";
+            AnnotationReply annotationReply = client.getAnnotationReply(action, jobId);
+            System.out.println("查询标注回复初始设置任务状态数据: " + annotationReply);
         }
     }
 }
